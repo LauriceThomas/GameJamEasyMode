23b5b55162a95ef35ce4ec457aaec8b47a5c189d
LauriceThomas <49695257+LauriceThomas@users.noreply.github.com>
2020-06-08 00:26:10 -0500

    Added Pause
    Has Pause and Restart capabilities.
    Has "skeleton" Movement - Not setup but started

---
+++ Game Jam/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static bool isPlayerDead;    //variable to be set in PlayerHealth
+    public float speed;                 //player movement speed
+    private float h;                    //horizontal axis
+    private float v;                    //vertical axis
+
+    
+    void Start()
+    {
+        h = 0f;
+        v = 0f;
+        isPlayerDead = false;
+    }
+
+ 
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //allow up/down float controls but no jump
+        if (isPlayerDead)
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            {
+
+            }
+        
+    }
+        //jump enabled but not up/down float
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+            {
+                CheckIfGrounded();
+            }
+        }
+
+        //left and right controls stay the same either way
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+
+    }
+
+    public void CheckIfGrounded()
+    {
+
+    }
+}
