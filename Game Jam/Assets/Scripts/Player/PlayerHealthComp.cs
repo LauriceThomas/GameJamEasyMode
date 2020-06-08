6254ff1c6d97bf0d08999bda5ee50c3f03f96c0f
Joaquin Ramirez <joaquinramirez97@yahoo.com>
2020-06-07 21:07:57 -0500

    Branch Tavera

--- Game Jam/Assets/Scripts/Player/PlayerHealthComp.cs
+++ Game Jam/Assets/Scripts/Player/PlayerHealthComp.cs
@@ -7,7 +7,7 @@ public class PlayerHealthComp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Player health");
+        Debug.Log("change");
     }
 
     // Update is called once per frame
