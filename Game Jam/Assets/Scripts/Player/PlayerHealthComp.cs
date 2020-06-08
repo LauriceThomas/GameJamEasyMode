acd022b6a70e4c47ec2ea9e6a5f69c16242e528b
Joaquin Ramirez <joaquinramirez97@yahoo.com>
2020-06-07 21:36:31 -0500

    Inserted Health Component basics

--- Game Jam/Assets/Scripts/Player/PlayerHealthComp.cs
+++ Game Jam/Assets/Scripts/Player/PlayerHealthComp.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthComp : MonoBehaviour
 {
+    public float health;
+    public float maxHealth = 100;
+    public Image healthbar;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("change");
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            ReceiveDamage(10);
+
+        if (healthbar)
+        {
+            healthbar.fillAmount = health / maxHealth;
+            healthbar.color = (health > 25 ? Color.green : Color.red);
+        }
+    }
+
+    public void ReceiveDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    public void GainHealth(float regenHealth)
+    {
+        health += regenHealth;
     }
 }
